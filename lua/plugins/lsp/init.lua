9e56b1a74cf1a90b5a5561a66761ef2eb0569cfd
Brian Sajeev <briansajeev@gmail.com>
2023-12-26 20:34:36 +0530

    Again tried my lazy oriented modification of lsp-zero's guide to set up lsp, still not working

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -7,139 +7,106 @@ local others = {}
 local mason_nonlsp_pkgs = vim.tbl_extend( "keep", linters, formatters, others)
 local mason_all_pkgs = vim.tbl_extend( "keep", mlsp_server_names, mason_nonlsp_pkgs)
 
--- local Util = require("lazyvim.util")
-local mlsp_server_configs = {}
-for i, server_name in ipairs(mlsp_server_names) do
-	mlsp_server_configs[server_name] = {}
+lsp_on_attach = function(event)
+	local opts = {buffer = event.buf}
+	vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+	vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+	vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+	vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+	vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+	vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+	vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+	vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+	vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+	vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+	vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+	vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+	vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts) 
 end
+
 
 return {
 
+	-- nvim-cmp for autocompletions
+	-- depends on it's sources being already loaded
+	-- calls lspconfig setup
+	{
+		'hrsh7th/nvim-cmp',
+		event = { "VeryLazy", "InsertEnter", },
+		dependencies = {
+			'neovim/nvim-lspconfig',
+		},
+		config = function()
+			local cmp = require('cmp')
+			cmp.setup {
+				sources = {
+					{name = 'nvim_lsp'}, --mention other additional sources like copilot, etc
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- enter key confirms completion item
+					['<CR>'] = cmp.mapping.confirm({select = false}),
+
+					-- ctrl + space triggers completion menu
+					['<C-Space>'] = cmp.mapping.complete(),
+				}),
+				-- snippet = {
+				--	expand = function(args)
+				--		require('luasnip').lsp_expand(args.body)
+				--	end,
+				-- },
+			}
+		end,
+	},
+
+	-- lsp config
+	-- calls mason etc
 	{
 		'neovim/nvim-lspconfig',
 		event = "VeryLazy",
 		dependencies = {
-			{ "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
 			{ "folke/neodev.nvim", opts = {} },
 			"mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
 		},
+		config = function()
+			local lspconfig = require('lspconfig')
+			local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-		opts = {
-			-- options for vim.diagnostic.config()
-			diagnostics = {
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-			},
-			inlay_hints = {
-				enabled = false,
-			},
-			servers = mlsp_server_configs,
-			setup = {}, -- You can add more configs here as <server_name> = { other settings } pairs.
-		},
+			vim.api.nvim_create_autocmd('LspAttach', {
+				desc = 'LSP actions',
+				callback = lsp_on_attach,
+			})
 
-		config = function(_, opts)
-
-			local servers = opts.servers
-
-			-- For cmp
-			local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				has_cmp and cmp_nvim_lsp.default_capabilities() or {},
-				opts.capabilities or {}
-			)
-
-			local function setup(server)
-				local server_opts = vim.tbl_deep_extend("force", {
-					capabilities = vim.deepcopy(capabilities),
-				}, servers[server] or {})
-
-				if opts.setup[server] then
-					if opts.setup[server](server, server_opts) then
-						return
-					end
-				elseif opts.setup["*"] then
-					if opts.setup["*"](server, server_opts) then
-						return
-					end
-				end
-				require("lspconfig")[server].setup(server_opts)
-			end
-
-			-- get all the servers that are available through mason-lspconfig
-			local have_mason, mlsp = pcall(require, "mason-lspconfig")
-			local all_mslp_servers = {}
-			if have_mason then
-				all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-			end
-
-			local ensure_installed = {} ---@type string[]
-			for server, server_opts in pairs(servers) do
-				if server_opts then
-					server_opts = server_opts == true and {} or server_opts
-					-- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-					if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
-						-- does lsp setup for non-mason servers
-						setup(server)
-					else
-						ensure_installed[#ensure_installed + 1] = server
-					end
-				end
-			end
-
-			if have_mason then
-				mlsp.setup({
-						ensure_installed = ensure_installed,
-						handlers = { setup } , -- This handles lspconfig[server].setup for mason installed servers
-						automatic_installation = true,
+			local default_setup = function(server)
+				lspconfig[server].setup({
+				capabilities = lsp_capabilities,
 				})
 			end
 
-
-
-
+			require('mason-lspconfig').setup({
+			ensure_installed = mslp_server_names,
+			handlers = {default_setup},
+			})
 		end
+	},
+
+	 {
+		"williamboman/mason-lspconfig.nvim",
+		-- event = "verylazy",
+		dependencies = "mason.nvim",
+		config = function() end,
+
 	},
 
 	{
 		"williamboman/mason.nvim",
 		cmd = "Mason",
-		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-		build = ":MasonUpdate",
+		keys = { { "<leader>cm", "<cmd>mason<cr>", desc = "mason" } },
+		build = ":masonupdate",
 		opts = {
 			ensure_installed = mason_nonlsp_pkgs,
 		},
-		---@param opts MasonSettings | {ensure_installed: string[]}
-		config = function(_, opts)
-			require("mason").setup(opts)
-			local mr = require("mason-registry")
-			mr:on("package:install:success", function()
-				vim.defer_fn(function()
-					-- trigger FileType event to possibly load this newly installed LSP server
-					require("lazy.core.handler.event").trigger({
-						event = "FileType",
-						buf = vim.api.nvim_get_current_buf(),
-					})
-				end, 100)
-			end)
-			local function ensure_installed()
-				for _, tool in ipairs(opts.ensure_installed) do
-					local p = mr.get_package(tool)
-					if not p:is_installed() then
-						p:install()
-					end
-				end
-			end
-			if mr.refresh then
-				mr.refresh(ensure_installed)
-			else
-				ensure_installed()
-			end
-		end,
 	},
-
 }
