568b9c303d1b29f8af284b96b313d2dc347079d4
Brian Sajeev <briansajeev@gmail.com>
2023-12-27 13:50:42 +0530

    Added CMP rounded window borders

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -113,6 +113,12 @@ return {
 						require('luasnip').lsp_expand(args.body)
 					end,
 				},
+
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				}
+
 			}
 		end,
 	},
