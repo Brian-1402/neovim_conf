183879f97e633239a24937836ca8de85faaf7160
Brian Sajeev <briansajeev@gmail.com>
2024-02-26 18:13:26 +0530

    Set up project switching using telescope, and project rooting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,12 +12,42 @@ return {
 			-- },
 		},
 
+		keys = {
+			{ -- lazy style key map
+			"<leader>ff",
+			"<cmd>Telescope find_files<cr>",
+			desc = "Telescope: find files",
+			},
+			{ -- lazy style key map
+			"<leader>fg",
+			"<cmd>Telescope live_grep<cr>",
+			desc = "Telescope: live grep",
+			},
+			{ -- lazy style key map
+			"<leader>fb",
+			"<cmd>Telescope buffers<cr>",
+			desc = "Telescope: buffers",
+			},
+			{ -- lazy style key map
+			"<leader>fh",
+			"<cmd>Telescope help_tags<cr>",
+			desc = "Telescope: help tags",
+			},
+			{ -- lazy style key map
+			"<leader>fp",
+			"<cmd>Telescope projects<cr>",
+			desc = "Telescope: projects",
+			},
+		},
+
 		config = function (_, opts)
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+			-- local builtin = require('telescope.builtin')
+			-- local extensions = require'telescope'.extensions
+			-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+			-- vim.keymap.set('n', '<leader>fp', extensions.projects.projects, {})
 			require('telescope').setup(opts)
 		end
 	},
@@ -61,6 +91,7 @@ return {
 	},
 
 	{
+		-- Automatically cd to project root. This supports telescope extension
 		"ahmedkhalf/project.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim", },
 		event = "VeryLazy",
@@ -97,5 +128,4 @@ return {
 			require("telescope").load_extension("agrolens")
 		end,
 	},
-
 }
